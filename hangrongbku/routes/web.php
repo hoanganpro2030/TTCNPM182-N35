11b96a4c5d93f8c0fd1d9207496d20a0ad302501
vokhactri <outofmilkyway@gmail.com>
2019-04-20 22:24:15 +0700

    add shopping_cart route

--- hangrongbku/routes/web.php
+++ hangrongbku/routes/web.php
@@ -21,3 +21,6 @@ Route::get('/hangrongbku',[
 Route::get('/hangrongbku/products',function(){
 	return view('template.pages.product');
 });
+Route::get('/hangrongbku/shoppingcart',function(){
+	return view('template.pages.shopping_cart');
+});
