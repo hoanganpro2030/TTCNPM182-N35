8d8806be71f06a34efd7e9667e6a79e04932c000
viethung98 <viethung.cse@gmail.com>
2019-05-04 16:34:30 +0700

    update

---
+++ hangrongbku/resources/views/template/pages/upload_product.blade.php
@@ -0,0 +1,41 @@
+@extends('template.master')
+@section('content')
+<div class="container">
+	<div class ="main-content">
+		<div class="row">
+			@include('template.user_option')
+			<div class=" col-sm-9 offset-2">
+				<form>
+					<h4>Đăng bán sản phẩm</h4>
+					<div class="space20">&nbsp;</div>
+					<div class="space20">&nbsp;</div>
+				  	<div class="form-group">
+					    <label for="exampleInputEmail1">Tên sản phẩm</label>
+					    <input type="text" class="form-control col-sm-5" id="exampleInputPassword1" placeholder="Nguyễn Văn A">
+				  	</div>
+				  	<div class="space20">&nbsp;</div>
+				  	<div class="form-group">
+					    <label for="exampleInputPassword1">Loại sản phẩm</label>
+					    <input type="text" class="form-control col-sm-5" id="exampleInputPassword1" placeholder="Nguyễn Văn A">
+				  	</div>
+				  	<div class="space20">&nbsp;</div>
+				  	<div class="form-group">
+					    <label for="exampleInputPassword1">Giá bán</label>
+                        <input type="text" class="form-control col-sm-5" id="exampleInputPassword1" placeholder="Nguyễn Văn A">				  	</div>
+				  	<div class="space20">&nbsp;</div>
+				  	<div class="form-group">
+					    <label for="exampleInputPassword1">Số lượng</label>
+                        <input type="text" class="form-control col-sm-5" id="exampleInputPassword1" placeholder="Nguyễn Văn A">				  	</div>
+				  	<div class="space20">&nbsp;</div>
+				  	<div class="form-group">
+					    <label for="exampleInputPassword1">Mô tả sản phẩm</label>
+                        <input type="text" class="form-control col-sm-5" id="exampleInputPassword1" placeholder="Nguyễn Văn A">				  	</div>
+				  	<div class="space20">&nbsp;</div>
+				  	<button type="submit" class="btn btn-primary">Cập nhật</button>
+				</form>
+			</div> <!-- #content -->
+		</div>
+	</div>
+	
+</div> <!-- .container -->
+@endsection
