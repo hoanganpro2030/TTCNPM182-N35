b67e070ee0fb509a1b8a1f02f4923851c4ff89db
vokhactri <outofmilkyway@gmail.com>
2019-05-05 15:10:33 +0700

    small bug fixed

--- hangrongbku/resources/views/template/user_option.blade.php
+++ hangrongbku/resources/views/template/user_option.blade.php
@@ -2,7 +2,7 @@
 <div class="col-sm-3">
 	<ul class="aside-menu">
 		<li><a href="#">Tài khoản của bạn</a></li>
-	<li><a href="{{route('user.inform')}}">Thông tin tài khoản</a></li>
+		<li><a href="{{route('user.inform')}}">Thông tin tài khoản</a></li>
 		<li><a href="#">Lịch sử đặt hàng</a></li>
 		<li><a href="{{route('productuser')}}">Sản phẩm đang bán</a></li>
 		<li><a href="#">Đăng hàng</a></li>
